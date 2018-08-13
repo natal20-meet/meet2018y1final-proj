93e2610ea7497877fae285f55d6fc730a81d64f2
natal20-meet <natal20@meet.mit.edu>
2018-08-13 07:32:44 -0400

    Stopping here

---
+++ natal20_final-proj.py
@@ -0,0 +1,45 @@
+import turtle
+
+#this inserts all pictures to the file
+turtle.register_shape("bg.gif")
+turtle.setup(1000,1000)
+
+
+turtle.shape("square")
+#this sets pictures as turtles.
+pic1 = turtle.clone()
+pic2 = turtle.clone()
+pic3 = turtle.clone()
+pic4 = turtle.clone()
+pic5 = turtle.clone()
+pic6 = turtle.clone()
+pic7 = turtle.clone()
+pic8 = turtle.clone()
+pic9 = turtle.clone()
+pic10 = turtle.clone()
+pic11 = turtle.clone()
+pic12 = turtle.clone()
+pic13 = turtle.clone()
+pic14 = turtle.clone()
+pic15 = turtle.clone()
+pic16 = turtle.clone()
+
+##turtle.bgpic("bg.gif")#sets the background as squares
+turtle.penup()
+turtle.goto(0,250)
+turtle.pendown()
+turtle.goto(250,250)
+turtle.goto(250,500)
+turtle.goto(0,500)
+turtle.goto(0,0)
+turtle.goto(250,0)
+turtle.goto(250,250)
+turtle.goto(500,250)
+
+
+
+
+
+
+turtle.mainloop() 
+
