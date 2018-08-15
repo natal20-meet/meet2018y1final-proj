cb6532759925b59d6958032c0ea7a73ac6c0e70a
natal20-meet <natal20@meet.mit.edu>
2018-08-15 10:01:14 -0400

    Stopping here

--- natal20_final-proj.py
+++ natal20_final-proj.py
@@ -1,6 +1,17 @@
 import turtle
 
+turtle.setup(1000,1000)#window size
+
 #this inserts all pictures to the file
+turtle.register_shape("gamecard.gif")
+turtle.register_shape("compost.gif")
+turtle.register_shape("airpollution.gif")
+turtle.register_shape("plasticwaste.gif")
+turtle.register_shape("globalwarming.gif")
+turtle.register_shape("greenearth.gif")
+turtle.register_shape("waterpollution1.gif")
+turtle.register_shape("leaf.gif")
+turtle.register_shape("poorturtle.gif")
 
 turtle.shape("square")
 #this sets pictures as turtles.
@@ -76,33 +87,38 @@ pic16.pendown()
 #lists 
 ##pos_list[]
 #variables
-pic_size = 300
+#pic_size = 300
 SIZE_X = 1000
 SIZE_Y = 1000
 
-# this function changes to the card when user clicks
-turtle.register_shape("gamecard.gif")
-turtle.setup(1000,1000)
-turtle.register_shape("compost.gif")
-turtle.register_shape("airpollution.gif")
-turtele.register_shape()
-turtle.register_shape("globalwarming.gif")
-turtle.register_shape("greenearth.gif")
-turtle.register_shape("waterpollution1.gif")
-turtle.register_shape("leaf.gif")
-turtle.register_shape("poorturtle.gif")
+flip = [False] * 17
 
+
+# this function changes to the card when user clicks
+
+#this sets the images as cards
 def compost_flip1(x, y):
+    flip[1] = True #sets the false as true
     pic1.shape("compost.gif")
+    if flip[13]: 
+        print('the fact')
+        pic1.hideturtle()
+        pic13.hideturtle()
 def compost_flip2(x,y):
+    flip[13] = True #complementary to the other function 
     pic13.shape("compost.gif")
+    if flip[1]:
+        print('the fact')
+        pic1.hideturtle()
+        pic13.hideturtle()
 def globalw_flip1(x,y):
-    pic3.shape("glabalwarming.gif")
+    pic3.shape("globalwarming.gif")
+
 def globalw_flip2(x,y):
-    pic10.shape("glabalwarming.gif")
+    pic10.shape("globalwarming.gif")
 def airpollution_flip1(x,y):
     pic4.shape("airpollution.gif")
-def airpolution_flip2(x,y):
+def airpollution_flip2(x,y):
     pic12.shape("airpollution.gif")
 def greenearth_flip1(x,y):
     pic6.shape("greenearth.gif")
@@ -120,11 +136,29 @@ def poor_flip1(x,y):
     pic5.shape("poorturtle.gif")
 def poor_flip2(x,y):
     pic8.shape("poorturtle.gif")
+def plastic_flip1(x,y):
+    pic11.shape("plasticwaste.gif")
+def plastic_flip2(x,y):
+    pic16.shape("plasticwaste.gif")
+    
     
 pic1.onclick(compost_flip1)
 pic13.onclick(compost_flip2)
 pic3.onclick(globalw_flip1)
 pic10.onclick(globalw_flip2)
+pic4.onclick(airpollution_flip1)
+pic12.onclick(airpollution_flip2)
+pic6.onclick(greenearth_flip1)
+pic15.onclick(greenearth_flip2)
+pic2.onclick(wp_flip1)
+pic9.onclick(wp_flip2)
+pic7.onclick(leaf_flip1)
+pic14.onclick(leaf_flip2)
+pic5.onclick(poor_flip1)
+pic8.onclick(poor_flip2)
+pic11.onclick(plastic_flip1)
+pic16.onclick(plastic_flip2)
+
 
 ##def place_pics():
 ##    min_x=-int(SIZE_X/2/pic_size)+1
@@ -133,21 +167,7 @@ pic10.onclick(globalw_flip2)
 ##    max_y=int(SIZE_Y/2/pic_size)+1
 ##    pic_x = random.randint(min_x,max_x)*pic_size
 ##    pic_y = random.randint(min_y,max_y)*pic_size 
-    
-    
-    
-
-if pic1.onclick + pic13.onclick == True:
-    write("the fact")
-    pic1.hidetrutle()
-    pic13.hideturtle()
-
-
-
-
-
-
-
+          
 
 
 
